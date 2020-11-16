861c79d883ece499be12287d21fb9312c1e75484
venky533 <66678600+venky533@users.noreply.github.com>
2020-11-16 09:37:55 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""⚡ JOIN TORRENT MIRROR  GROUP\n\n 👉Link: <a href="https://t.me/MaxxMirror">MAXX MIRROR GROUP</a>""", disable_web_page_preview=True)
+    await message.reply_text("""⚡ JOIN TORRENT MIRROR  GROUP\n\n 👉Link: <a href="https://t.me/SBZGDRIVE">SUPER BOTZ GDrive </a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
